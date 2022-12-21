ba49a72f80a465e192976a6c369200c0db96fa18
halilgoksu <halilskywater@gmail.com>
2022-12-21 01:39:42 -0700

    fire

--- .history/utils/firebase_20221221013048.js
+++ .history/utils/firebase_20221221013048.js
@@ -3,7 +3,8 @@ import { initializeApp } from "firebase/app";
 
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+import { getAnalytics } from "firebase/analytics";
+//hig added getAnltic 
 
 
 // TODO: Add SDKs for Firebase products that you want to use
